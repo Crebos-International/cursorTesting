bb246d31b4e3cf8d9519b55ae317e43e2d678f9e
himankcrebos <himank.crebos@gmail.com>
2025-07-02 17:46:25 +0530

    Initial Commit

---
+++ CusrsorTesting/CusrsorTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CusrsorTestingApp.swift
+//  CusrsorTesting
+//
+//  Created by Himank Bansal on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CusrsorTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
